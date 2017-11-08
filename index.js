31812e49382830ae15659272ad0c3f5aaf0dc143
Ben Conolly <bconolly@atlassian.com>
2017-11-08 13:58:58 +1100

    display comment descriptions

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ module.exports = function extractReactTypesLoader(content /* : string */) {
   const filename = this.resource;
   const ext = path.extname(filename);
   const typeSystem = ext === '.ts' || ext === '.tsx' ? 'typescript' : 'flow';
-  const types = extractReactTypes(content, typeSystem);
+  const types = extractReactTypes(content, typeSystem, filename);
   return `module.exports = ${JSON.stringify(types)}`;
 };
