4d3a85f4629f03b3c542bfd321ac5b9298fd5370
Ben Conolly <ben@thinkmill.com.au>
2018-11-21 11:42:33 +1100

    updates to website dependencies

--- index.js
+++ index.js
@@ -3,12 +3,46 @@
 const path = require('path');
 const extractReactTypes = require('extract-react-types');
 
+const devProps = {
+  classes: [
+    {
+      value: {
+        kind: 'object',
+        members: [
+          {
+            kind: 'property',
+            key: { kind: 'id', name: 'Warning' },
+            value: { kind: 'any' },
+            optional: false,
+            leadingComments: [
+              {
+                type: 'commentBlock',
+                value: `extract-react-types is not being run in dev mode for speed reasons. If you need to
+see prop types add the environment variable \`FORCE_EXTRACT_REACT_TYPES\`
+eg:
+- \`FORCE_EXTRACT_REACT_TYPES=true yarn start <packageName>\`
+- \`FORCE_EXTRACT_REACT_TYPES=true yarn start:<team>\``,
+                raw: '**',
+              },
+            ],
+            default: {
+              kind: 'string',
+              value: 'Prop types are not shown in dev mode',
+            },
+          },
+        ],
+        referenceIdName: 'AvatarPropTypes',
+      },
+    },
+  ],
+};
+
 module.exports = function extractReactTypesLoader(content /* : string */) {
   if (
     !['staging', 'production'].includes(process.env.WEBSITE_ENV) &&
     !process.env.FORCE_EXTRACT_REACT_TYPES
   ) {
-    return 'module.exports = {}';
+    return `module.exports = ${JSON.stringify(devProps)}`;
   }
 
   const filename = this.resource;
