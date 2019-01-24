bed8c5286cf7d887bf6673a984860931367ad7f8
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2019-01-24 23:23:14 +1100

    Single babel config + update the snapshot tests (#46)
    * combined the babel config and added test script in the root of the project
    
    * Do not run test inside k2s prepublish
    
    * updated the snapshot tests for forward ref and memo

--- packages/pretty-proptypes/setup-test-env.js
+++
@@ -1,9 +0,0 @@
-require('babel-register');
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-let jsDom = (new JSDOM(`...`)).window
-global.window = jsDom.window;
-global.document = window.document
-global.navigator = window.navigator;
