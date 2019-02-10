559051d36740a745ab9a54eae0c183e8f9b25bad
Ajay Narain Mathur <amathur@atlassian.com>
2019-02-10 18:43:44 +1100

    added react-router-dom

--- website/src/index.js
+++ website/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Website() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Website />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- website/src/uriUtils.js
+++ website/src/uriUtils.js
@@ -5,3 +5,22 @@ export const compress = (string: string) =>
     .replace(/\+/g, '-') // Convert '+' to '-'
     .replace(/\//g, '_') // Convert '/' to '_'
     .replace(/=+$/, ''); // Remove ending '='
+
+export const decompress = (string: string) =>
+  LZString.decompressFromBase64(
+    string
+      .replace(/-/g, '+') // Convert '-' to '+'
+      .replace(/_/g, '/') // Convert '_' to '/'
+  );
+
+export const encode = (value: any) => window.encodeURIComponent(value);
+
+export const updateQuery = (state: PersistedState) => {
+  const query = Object.keys(state)
+    .map(key => {
+      return key === 'code' ? `${key}_lz=` + compress(state.code) : key + '=' + encode(state[key]);
+    })
+    .join('&');
+
+  window.location.hash = '?' + query;
+};
