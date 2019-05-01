854d11779ef361ce6999c0bdb865a9b729385b11
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2019-05-01 14:41:01 +1000

    Revert back to index.test.js

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,12 @@ module.exports = api => {
   // Cache the returned value forever and don't call this function again.
   api.cache(true);
 
-  // Return the value that will be cached.
   return {
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
     plugins: [
       'emotion',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-runtime'
-      // 'transform-object-rest-spread',
-      // 'transform-runtime',
-      // 'syntax-dynamic-import'
     ]
   };
 };
