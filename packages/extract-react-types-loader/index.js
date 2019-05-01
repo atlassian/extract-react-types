ccdc7162364a956b7f459992d2c41a3d4845e41c
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2019-05-01 15:01:06 +1000

    Require e-r-t for CJS with .default

--- packages/extract-react-types-loader/index.js
+++ packages/extract-react-types-loader/index.js
@@ -1,7 +1,7 @@
 // @flow
 
 const path = require('path');
-const extractReactTypes = require('extract-react-types');
+const extractReactTypes = require('extract-react-types').default;
 
 const devProps = {
   component: {
