3e891496799173f6ddd5b9f93baaf48a371cfcf2
Patrick Lai <plai@atlassian.com>
2019-05-06 14:07:07 +1000

    refactored converters to be more consistent with other packages

--- packages/generate-react-types/src/converters.js
+++ packages/generate-react-types/src/converters.js
@@ -2,68 +2,76 @@
 import * as t from '@babel/types';
 import generate from '@babel/generator';
 
-const id = (ertNode, convert) => t.identifier(ertNode.name);
-
-const boolean = (ertNode, convert) => t.tsBooleanKeyword();
-
-const generic = (ertNode, convert) => convert(ertNode.value);
-
-const object = (ertNode, convert) => {
-  return t.tsTypeLiteral(ertNode.members.map(n => convert(n, convert)));
-};
-
-const program = (ertNode, convert) => {
-  if (ertNode.component) {
-    return t.program([
-      t.importDeclaration(
-        [t.importDefaultSpecifier(t.identifier('React'))],
-        t.stringLiteral('react')
-      ),
-      t.exportNamedDeclaration(
-        {
-          ...t.variableDeclaration('var', [
-            t.variableDeclarator({
-              ...convert(ertNode.component.name, convert),
-              typeAnnotation: t.tsTypeAnnotation(
-                t.tsTypeReference(
-                  t.tsQualifiedName(t.identifier('React'), t.identifier('ComponentType')),
-                  t.tsTypeParameterInstantiation([convert(ertNode.component, convert)])
-                )
-              )
-            })
-          ]),
-          declare: true
-        },
-        []
-      )
-    ]);
-  }
-  return t.program([]);
-};
-
-const property = (ertNode, convert) => {
-  return {
-    ...t.tsPropertySignature(convert(ertNode.key), t.tsTypeAnnotation(convert(ertNode.value))),
-    optional: !!ertNode.default
-  };
-};
-
-const string = (ertNode, convert) => t.tsStringKeyword();
-
 const tsConverters = {
-  boolean,
-  id,
-  generic,
-  object,
-  program,
-  property,
-  string
+  id: ertNode => t.identifier(ertNode.name),
+
+  boolean: () => t.tsBooleanKeyword,
+
+  generic: (ertNode, convert) => convert(ertNode.value),
+
+  object: (ertNode, convert) => {
+    return t.tsTypeLiteral(ertNode.members.map(n => convert(n, convert)));
+  },
+
+  program: (ertNode, convert) => {
+    if (ertNode.component) {
+      return t.program([
+        t.importDeclaration(
+          [t.importDefaultSpecifier(t.identifier('React'))],
+          t.stringLiteral('react')
+        ),
+        t.exportNamedDeclaration(
+          {
+            ...t.variableDeclaration('var', [
+              t.variableDeclarator({
+                ...convert(ertNode.component.name, convert),
+                typeAnnotation: t.tsTypeAnnotation(
+                  t.tsTypeReference(
+                    t.tsQualifiedName(t.identifier('React'), t.identifier('ComponentType')),
+                    t.tsTypeParameterInstantiation([convert(ertNode.component, convert)])
+                  )
+                )
+              })
+            ]),
+            declare: true
+          },
+          []
+        )
+      ]);
+    }
+    return t.program([]);
+  },
+
+  property: (ertNode, convert) => {
+    return {
+      ...t.tsPropertySignature(convert(ertNode.key), t.tsTypeAnnotation(convert(ertNode.value))),
+      optional: !!ertNode.default
+    };
+  },
+
+  string: () => {
+    return t.tsStringKeyword();
+  },
+
+  number: () => {
+    return t.tsNumberKeyword();
+  },
+
+  union: (ertNode, convert) => {
+    const types = ertNode.types.map(type => convert(type, convert));
+    return t.tsUnionType(types);
+  }
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const convertToTs = ertNode => {
   let convert = node => {
-    return tsConverters[node.kind](node, convert);
+    if (node.kind in tsConverters) {
+      return tsConverters[node.kind](node, convert);
+    }
+    // eslint-disable-next-line no-console
+    console.error(`Cannot process type: ${node.kind} right now`);
   };
+
   return generate(convert(ertNode)).code;
 };
