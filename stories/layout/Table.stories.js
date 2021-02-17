4af63c2612206053d80ee0579394b59b522d83c7
Declan Warn <dwarn@atlassian.com>
2021-02-17 11:27:43 +1100

    Add stories for existing layouts

---
+++ stories/layout/Table.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { PropsTable } from 'pretty-proptypes';
+import TypeScriptComponent from '../TypeScriptComponent';
+
+export default {
+  title: 'Example/Layouts',
+  component: PropsTable
+};
+
+const Template = args => args.component;
+
+export const Table = Template.bind({});
+
+Table.args = {
+  component: <PropsTable component={TypeScriptComponent} heading="Primitive types" />
+};
