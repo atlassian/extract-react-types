78d547407d26ed489e26915d4314e886e4b72d80
Declan Warn <dwarn@atlassian.com>
2021-03-05 15:02:21 +1100

    Update stories

--- stories/layout/Hybrid.stories.js
+++ stories/layout/Hybrid.stories.js
@@ -13,109 +13,20 @@ export default {
 
 const Template = args => args.component;
 
-export const Hybrid = Template.bind({});
+export const Base = Template.bind({});
 
-Hybrid.args = {
+Base.args = {
   component: <HybridLayout component={TypeScriptComponent} heading="Primitive types" />
 };
 
-export const Option1 = Template.bind({});
+export const WithReset = Template.bind({});
 
-Option1.args = {
+WithReset.args = {
   component: (
     <>
       <Global
         styles={css`
           @import 'https://unpkg.com/@atlaskit/css-reset@6.0.5/dist/bundle.css';
-
-          #root {
-            tbody {
-              border: none;
-            }
-
-            h3 {
-              padding-bottom: 8px;
-              border-bottom-color: #ebecf0;
-            }
-
-            h3 code:first-of-type {
-              background-color: rgb(244, 245, 247);
-              color: rgb(23, 43, 77);
-              border-radius: 3px;
-              padding: 4px;
-              font-size: initial;
-            }
-          }
-        `}
-      />
-      <HybridLayout component={TypeScriptComponent} heading="Primitive types" />
-    </>
-  )
-};
-
-export const Option5 = Template.bind({});
-
-Option5.args = {
-  component: (
-    <>
-      <Global
-        styles={css`
-          @import 'https://unpkg.com/@atlaskit/css-reset@6.0.5/dist/bundle.css';
-
-          #root {
-            tbody {
-              background: #fafbfc;
-              border-bottom-color: #ebecf0;
-            }
-
-            h3 {
-              padding-left: 8px;
-              border-bottom-color: #ebecf0;
-            }
-
-            th {
-              padding-left: 8px;
-            }
-          }
-        `}
-      />
-      <HybridLayout component={TypeScriptComponent} heading="Primitive types" />
-    </>
-  )
-};
-
-export const Option8 = Template.bind({});
-
-Option8.args = {
-  component: (
-    <>
-      <Global
-        styles={css`
-          @import 'https://unpkg.com/@atlaskit/css-reset@6.0.5/dist/bundle.css';
-
-          #root {
-            tbody {
-              border: none;
-            }
-
-            th {
-              text-align: right;
-              padding-left: 24px;
-            }
-
-            h3 {
-              padding-bottom: 8px;
-              border-bottom-color: #ebecf0;
-            }
-
-            h3 code:first-of-type {
-              background-color: rgb(244, 245, 247);
-              color: rgb(23, 43, 77);
-              border-radius: 3px;
-              padding: 4px;
-              font-size: initial;
-            }
-          }
         `}
       />
       <HybridLayout component={TypeScriptComponent} heading="Primitive types" />
